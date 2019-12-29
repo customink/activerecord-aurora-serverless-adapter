4ae9b161e4897bded1f3dbdf7687e346038a90d3
Ken Collins <ken@metaskills.net>
2019-12-28 21:26:12 -0500

    Longer delay for minitest retry.

--- test/support/aasa_minitest.rb
+++ test/support/aasa_minitest.rb
@@ -5,19 +5,19 @@ module AASA
       defined?(::ActiveRecord) && defined?(::ARUnit2Model)
     end
 
-    def retry_foreign_key_checks
+    def retry_foreign_key_checks(count)
       return unless retry_foreign_key_checks?
-      reset_foreign_key_check_for_class(ActiveRecord::Base)
-      reset_foreign_key_check_for_class(ARUnit2Model)
+      reset_foreign_key_check_for_class ActiveRecord::Base, count
+      reset_foreign_key_check_for_class ARUnit2Model, count
     end
 
-    def reset_foreign_key_check_for_class(model)
+    def reset_foreign_key_check_for_class(model, count)
       c = model.connection
       return unless c
       v = c.query_value "SELECT @@FOREIGN_KEY_CHECKS"
       puts "[RETRY-FOREIGN_KEY_CHECKS] current: #{v}"
       c.update "SET FOREIGN_KEY_CHECKS = 1"
-      sleep 0.5
+      sleep(count)
     end
 
     extend self
@@ -35,5 +35,5 @@ Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new()]
 #
 Minitest::Retry.use! retry_count: 3, verbose: true
 Minitest::Retry.on_retry do |klass, test_name, retry_count|
-  AASA::Mtr.retry_foreign_key_checks
+  AASA::Mtr.retry_foreign_key_checks(retry_count)
 end
